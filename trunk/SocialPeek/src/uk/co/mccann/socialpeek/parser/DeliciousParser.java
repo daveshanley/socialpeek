9a538854674f3cdeaaef11a9561824cf178a89e8
theshanman <theshanman@fc632ebe-6448-0410-a038-81efc3b9b1fe>
2008-03-20 11:06:11 +0000

    added in we feel fine service

--- trunk/SocialPeek/src/uk/co/mccann/socialpeek/parser/DeliciousParser.java
+++ trunk/SocialPeek/src/uk/co/mccann/socialpeek/parser/DeliciousParser.java
@@ -1,5 +1,6 @@
 package uk.co.mccann.socialpeek.parser;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -16,13 +17,16 @@ import uk.co.mccann.socialpeek.model.SocialService;
 import uk.co.mccann.socialpeek.service.DeliciousService;
 import yarfraw.core.datamodel.ChannelFeed;
 import yarfraw.core.datamodel.ItemEntry;
+import yarfraw.core.datamodel.YarfrawException;
 import yarfraw.io.FeedReader;
+import yarfraw.io.FeedWriter;
 
 public class DeliciousParser extends AbstractParser implements Parser {
 	
 	private FeedReader reader;
 	private ChannelFeed channel;
 	private SimpleDateFormat deliciousDateFormat;
+	private long expireLengthMillis = 1800000; // 30  minutes;.
 	
 	/**
      *  Default constructor, calls super constructor from AbstractParser
@@ -32,30 +36,120 @@ public class DeliciousParser extends AbstractParser implements Parser {
 		super(service);
 	}
 	
+	private boolean checkRSSCachedFileValid(File file) {
+		
+		if(file.exists()) {
+			
+			long time = System.currentTimeMillis();
+			if(file.lastModified() > (time - this.expireLengthMillis)) {
+				
+				return true;
+				
+			} else {
+				
+				return false;
+			}
+			
+		} else {
+			
+			return false;
+		}
+		
+	}
+	
+	private void createRSSCachedFile(File file, ChannelFeed feed) throws ParseException {
+	
+		try {
+			
+			FeedWriter w = new FeedWriter(file);
+			w.writeChannel(feed);
+		
+		} catch (YarfrawException e) {
+		
+			throw new ParseException("unable to write file: " + e.getMessage());
+		}
+		
+		
+	}
+	
+	
 	public DeliciousParser() { }
 	
 	
 	public Data getKeywordItem(String keyword) throws ParseException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		try {
+			
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.key."+ keyword.toLowerCase() + ".xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/tag/" + keyword));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
+				
+				
+			/* get a list of RSS items and then shuffle them up for a random peek! */
+			List<ItemEntry> items = this.channel.getItems();
+			
+			/* shuffle it up for some randomness */
+			Collections.shuffle(items);
+			
+			return this.compileDeliciousData(items.get(this.random.nextInt(items.size()-1)));
+			
+			
+		} catch (Exception exp) {
+			exp.printStackTrace();
+			throw new ParseException("unable to parse del.icio.us RSS data: " + exp.getMessage());
+			
+		}
+		
+		
 	}
 
-	public List<Data> getLatestMultipleUserItems(int userId, int limit)
-			throws ParseException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Data> getLatestMultipleUserItems(int userId, int limit) throws ParseException {
+		return this.getLatestMultipleUserItems(String.valueOf(userId), limit);
 	}
 
 	public List<Data> getLatestMultipleUserItems(String userId, int limit) throws ParseException {
 		
 		try {
 			
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.user."+ userId.toLowerCase() + ".xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
+			
 			List<Data> extractedData = new ArrayList<Data>();
 			
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
-			this.channel = this.reader.readChannel();   
-		
+			
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
 			
@@ -96,10 +190,27 @@ public class DeliciousParser extends AbstractParser implements Parser {
 		
 		try {
 			
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/recent"));
-			this.channel = this.reader.readChannel();   
-		
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.user."+ userId.toLowerCase() + ".xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
+			
+			
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
 			
@@ -121,9 +232,26 @@ public class DeliciousParser extends AbstractParser implements Parser {
 			
 			List<Data> extractedData = new ArrayList<Data>();
 			
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/recent"));
-			this.channel = this.reader.readChannel();   
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.recent.xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/recent"));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
+			
 		
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
@@ -164,10 +292,27 @@ public class DeliciousParser extends AbstractParser implements Parser {
 			
 			List<Data> extractedData = new ArrayList<Data>();
 			
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/tag/" + keyword));
-			this.channel = this.reader.readChannel();   
-		
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.key."+ keyword.toLowerCase() + ".xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/tag/" + keyword));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
+			
+			
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
 			
@@ -201,10 +346,76 @@ public class DeliciousParser extends AbstractParser implements Parser {
 		
 	}
 
-	public List<Data> getMultipleKeywordItems(String[] keywords, int limit)
-			throws ParseException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Data> getMultipleKeywordItems(String[] keywords, int limit) throws ParseException {
+		
+		try {
+			
+			/* first we need to work out the ratio of keywords to limited returns */
+			int ratio = limit  / keywords.length;
+			
+			List<Data> extractedData = new ArrayList<Data>();
+			
+			for(int x = 0; x < keywords.length; x++) {
+			
+				/* check cache file */
+				File cachedRSSFile = new File("rssdata/delicious.rss.key."+ keywords[x] + ".xml");
+				
+				if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+				
+					/* set up a new RSS reader from source */
+					this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/tag/" + keywords[x]));
+					this.channel = this.reader.readChannel();   
+					
+					/* write file to cache */
+					this.createRSSCachedFile(cachedRSSFile, this.channel);
+				
+				} else {
+					
+					/* set up a new RSS reader from file */
+					this.reader = new FeedReader(cachedRSSFile);
+					this.channel = this.reader.readChannel();   
+					
+				}
+				
+				/* get a list of RSS items and then shuffle them up for a random peek! */
+				List<ItemEntry> items = this.channel.getItems();
+				
+				/* shuffle them all up for good measure! */
+				Collections.shuffle(items);
+				
+				int counter = 0;
+				for(ItemEntry item : items) {
+					if(counter < ratio) {
+						/* compile item */
+						extractedData.add(this.compileDeliciousData(item));
+					} else {
+						break;
+					}
+					counter++;
+				}
+			}
+			
+			Collections.shuffle(extractedData);
+			
+			List<Data> compactedData = new ArrayList<Data>();
+			
+			/* now trim it up */
+			if(limit > extractedData.size()) limit = extractedData.size(); // make sure we don't go out of bounds!
+			for(int x = 0; x < limit; x++) {
+				compactedData.add(extractedData.get(x));
+			}
+			
+			return compactedData;
+			
+			
+		} catch (Exception exp) {
+			
+			throw new ParseException("unable to parse del.icio.us RSS data: " + exp.getMessage());
+			
+		}
+		
+		
+		
 	}
 
 	public List<Data> getMultipleUserItems(int userId, int limit) throws ParseException {
@@ -218,10 +429,27 @@ public class DeliciousParser extends AbstractParser implements Parser {
 			
 			List<Data> extractedData = new ArrayList<Data>();
 			
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
-			this.channel = this.reader.readChannel();   
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.user."+ userId.toLowerCase() + ".xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
 		
+			
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
 			
@@ -233,6 +461,9 @@ public class DeliciousParser extends AbstractParser implements Parser {
 			}
 			
 			List<Data> compactedData = new ArrayList<Data>();
+			
+			/* shuffle it up */
+			Collections.shuffle(extractedData);
 			
 			/* now trim it up */
 			if(limit > extractedData.size()) limit = extractedData.size(); // make sure we don't go out of bounds!
@@ -253,6 +484,8 @@ public class DeliciousParser extends AbstractParser implements Parser {
 	
 	private Data compileDeliciousData(ItemEntry rssItem) throws java.text.ParseException {
 		
+		this.setUpParser(); // re-set things!
+		
 		Data data = new PeekData();
 		
 		data.setBody(rssItem.getDescriptionOrSummaryText());
@@ -261,7 +494,20 @@ public class DeliciousParser extends AbstractParser implements Parser {
 		Calendar pubDate = Calendar.getInstance();
 		
 		/* parse the date */
-		pubDate.setTime(this.deliciousDateFormat.parse(rssItem.getPubDate()));
+		
+		try {
+		
+			/* try first publication date type */
+			pubDate.setTime(this.deliciousDateFormat.parse(rssItem.getPubDate()));
+		
+		} catch(java.text.ParseException exp) {
+			
+			
+			this.deliciousDateFormat = new SimpleDateFormat("yyyy-mm-dd'T'kk:mm:ss'+00:00'");
+			
+			/* try second publication date type */
+			pubDate.setTime(this.deliciousDateFormat.parse(rssItem.getPubDate()));
+		}
 		
 		data.setDate(pubDate);
 		data.setHeadline(rssItem.getTitleText());
@@ -275,10 +521,26 @@ public class DeliciousParser extends AbstractParser implements Parser {
 	public Data getSingleItem() throws ParseException {
 		
 		try {
-		
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/recent"));
-			this.channel = this.reader.readChannel();   
+			
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.recent.xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/recent"));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
 		
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
@@ -308,9 +570,25 @@ public class DeliciousParser extends AbstractParser implements Parser {
 	public Data getSingleUserItem(String userId) throws ParseException {
 		try {
 			
-			/* set up a new RSS reader */
-			this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
-			this.channel = this.reader.readChannel();   
+			/* check cache file */
+			File cachedRSSFile = new File("rssdata/delicious.rss.user."+ userId.toLowerCase() + ".xml");
+			
+			if(!this.checkRSSCachedFileValid(cachedRSSFile)) {
+			
+				/* set up a new RSS reader from source */
+				this.reader = new FeedReader(new HttpURL(DeliciousService.DELICIOUS_URL + "rss/" + userId));
+				this.channel = this.reader.readChannel();   
+				
+				/* write file to cache */
+				this.createRSSCachedFile(cachedRSSFile, this.channel);
+			
+			} else {
+				
+				/* set up a new RSS reader from file */
+				this.reader = new FeedReader(cachedRSSFile);
+				this.channel = this.reader.readChannel();   
+				
+			}
 		
 			/* get a list of RSS items and then shuffle them up for a random peek! */
 			List<ItemEntry> items = this.channel.getItems();
@@ -337,8 +615,10 @@ public class DeliciousParser extends AbstractParser implements Parser {
 	}
 
 	public Data getKeywordItem(String[] keywords) throws ParseException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Data> data = this.getMultipleKeywordItems(keywords, 20);
+		return data.get(this.random.nextInt(data.size()-1));
+	
 	}
 
 }
